16b02922f129c8acf464e8b3298ae2795e9545ff
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-05-24 00:16:22 +0530

    Update providers.tf

--- terraform-stg/providers.tf
+++ terraform-stg/providers.tf
@@ -10,9 +10,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "969d33f2-743f-45d4-9866-8e995f507836"
-  tenant_id       = "1ca85363-714b-432b-b048-5c7a9f896d62"
-  client_secret   = "l~M8Q~pyPfCfwXNAHfZN1rkXFrHkxRyQyC9TpbWI"
-  subscription_id = "a52b7487-b59c-45f4-b7ce-2a69748e9fc9"
+#   client_id       = "969d33f2-743f-45d4-9866-8e995f507836"
+#   tenant_id       = "1ca85363-714b-432b-b048-5c7a9f896d62"
+#   client_secret   = "l~M8Q~pyPfCfwXNAHfZN1rkXFrHkxRyQyC9TpbWI"
+#   subscription_id = "a52b7487-b59c-45f4-b7ce-2a69748e9fc9"
 
 }
